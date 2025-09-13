29cfa8f4d1fcb31f818ec6df79aa94136b220ce2
tomaslopezvasquez <tomaslopezvasquez@gmail.com>
2025-09-13 10:46:29 -0300

    tp1 p5js

---
+++ tp1/tp1.js
@@ -0,0 +1,23 @@
+//tomas lopez vasquez, comision 3, tp1
+//https://www.youtube.com/watch?v=ITnAO6fYkWU
+let colorRandom;
+let foto;
+function preload() {
+  foto = loadImage("data/ilusion.png");
+}
+
+function setup() {
+  createCanvas(800, 400);
+  colorRandom = color(0);
+}
+
+function draw() {
+  translate(400, 0);
+  background(255);
+  cuadrados(30); // función para generar los cuadrados
+  lineas();     // función para generar las líneas
+  image(foto, -400, 0, 400, 400);
+  strokeWeight(5);
+  fill(255);
+  rect(150, 150, 100, 100); // rectángulo blanco del medio
+}
